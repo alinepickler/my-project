adb2b527010a57ef5d1ec8aea3ee28e351e4789b
Aline Pickler <Aline.Pickler@nordnet.se>
2018-05-30 16:52:12 +0200

    add mapStateToProps to NewItem component

--- src/components/news-item/index.js
+++ src/components/news-item/index.js
@@ -1,44 +1,60 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+import PropTypes from "prop-types";
+import { distanceInWordsToNow } from "date-fns";
 import { Link } from "react-router-dom";
 import { api } from "../../utils";
 import "./styles.css";
 
-export class NewsItem extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      item: undefined
-    };
-  }
+class NewsItem extends Component {
   componentDidMount() {
-    api
-      .getItem(this.props.id)
-      .then(item => {
-        this.setState({ item });
-      })
-      .catch(err => {
-        console.error(err);
-      });
+    this.props.fetchItem(this.props.id);
   }
   render() {
-    const { item } = this.state;
+    const { item } = this.props;
     if (!item) {
       return <div>Loading...</div>;
     }
+    const dateTime = new Date(item.time * 1000);
+    const relativeTime = distanceInWordsToNow(dateTime);
     return (
-      <div className="item">
-        <h3 className="title">
-          <a className="link" href={item.url}>
-            {item.title}
-          </a>
-        </h3>
-        <Link to={`/item/${item.id}`}>{item.score} points </Link>
-        by {item.by} at {item.time} |{" "}
-        <Link to={`/item/${item.id}`}>
-          {item.descendants === 0 ? "discuss" : item.descendants + " comments"}
-        </Link>
+      <div>
+        <a href={item.url} className="newsItemTitle">
+          <h2 className="newsItem-heading">{item.title}</h2>
+        </a>
+        <div>
+          <Link to={`/item/${item.id}`} className="newsItem">
+            <span className="newsItem">{item.score} points | </span>
+          </Link>
+          <span className="newsItem">by {item.by} | </span>
+          <Link to={`/item/${item.id}`} className="newsItem">
+            <span className="newsItem">
+              <time dateTime={dateTime}>{relativeTime} ago | </time>
+            </span>
+            <span className="newsItem">
+              {item.descendants === 0
+                ? "discuss"
+                : item.descendants + " comments"}
+            </span>
+          </Link>
+        </div>
       </div>
     );
   }
 }
+NewsItem.propTypes = {
+  id: PropTypes.number.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    item: (state.data.items[ownProps.id] || {}).item
+  };
+};
+
+const mapDispatchToProps = {
+  fetchItem: actions.fetchItem
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewsItem);
